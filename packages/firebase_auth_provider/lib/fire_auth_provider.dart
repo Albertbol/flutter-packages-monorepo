43c944457ac41eecc7b8f639ffd6aedd0c693528
Albertbol <alb.bolush@gmail.com>
2022-06-21 08:20:46 +0200

    expose auth enum firebase auth

--- packages/firebase_auth_provider/lib/fire_auth_provider.dart
+++ packages/firebase_auth_provider/lib/fire_auth_provider.dart
@@ -6,5 +6,6 @@ library firebase_auth_provider;
 export 'src/public/public.dart';
 export 'src/auth/public/public.dart';
 export 'src/auth/controller.dart';
+export 'src/auth/enum.dart';
 
 // TODO: Export any libraries intended for clients of this package.
