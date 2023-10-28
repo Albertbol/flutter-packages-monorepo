9d06c939a3667bd7fda9d1ea045aac5a45392e6d
Albert <alb.bolush@gmail.com>
2023-10-28 15:14:49 +0200

    fix: remove comment

--- packages/firebase_crashlytics_provider/lib/fire_crashlytics_provider.dart
+++ packages/firebase_crashlytics_provider/lib/fire_crashlytics_provider.dart
@@ -1,6 +1,5 @@
 /// Support for doing something awesome.
 ///
-/// More dartdocs go here.
 library firebase_crashlytics_provider;
 
 export 'src/public.dart';
--- packages/getx_provider/lib/getx_provider.dart
+++ packages/getx_provider/lib/getx_provider.dart
@@ -1,6 +1,5 @@
 /// Support for doing something awesome.
 ///
-/// More dartdocs go here.
 library getx_provider;
 
 export 'src/public/public.dart';
